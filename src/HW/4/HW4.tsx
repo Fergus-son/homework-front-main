38004f529ce9dd5e2922fd8edc8b8b2615e81936
Fergus-son <sarapovnikolaj790@gmail.com>
2025-04-23 23:17:46 +0300

    Update HW4.tsx

--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Button } from './Button';
 import { Input } from './Input';
+import styles from './HW4.module.css';
 
 export const HW4 = () => {
   // 1️⃣ Раскомментируйте JSX (HW4.tsx) и вы увидите,
@@ -40,7 +41,7 @@ export const HW4 = () => {
       <ol id={'hw04-tasks'}>
         {texts.map((el, index) => {
           return (
-            <li key={index} id={`hw04-task-${index}`} className={el ? 'chetNechet' : ''}>
+            <li key={index} id={`hw04-task-${index}`} className={index % 2 !== 0 ? styles.chetNechet : ''}>
         { el }
             </li>
       );
